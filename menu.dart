f4aa749ad45224729b9b582d85603e9e932cba09
Agnaitaansw <agnaitaansw@gmail.com>
2026-09-08 11:36:36 +0700

    if ke switch

--- menu.dart
+++ menu.dart
@@ -7,21 +7,25 @@ void tampilkanMenu() {
     print("          MENU UTAMA");
     print("==============================");
     print("1. Data Kelompok");
-    print("2. menu penjumlahan");
-    print("3.menu input bilangan");
+    print("2. Perkalian");
+    print("3. Apaa ini");
     print("0. Keluar");
     print("==============================");
 
     stdout.write("Pilih menu : ");
     String? pilihan = stdin.readLineSync();
 
-    if (pilihan == "1") {
-      dataKelompok();
-    } else if (pilihan == "0") {
-      print("\nProgram selesai.");
-      break;
-    } else {
-      print("\nPilihan tidak tersedia!");
+    switch (pilihan) {
+      case "1":
+        dataKelompok();
+        break;
+
+      case "0":
+        print("Program selesai.");
+        return;
+
+      default:
+        print("Pilihan tidak tersedia.");
     }
   }
 }
